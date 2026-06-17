804d9f1c3f98c0a31affc7329396babc9df90677
Lucas <lojadskids@gmail.com>
2026-06-17 15:49:28 -0300

    fix: nome da aba para 'Worksheet' (padrão Yampi), link_foto_principal vazio

--- tools/gerar-planilha.js
+++ tools/gerar-planilha.js
@@ -162,7 +162,7 @@ function buildXlsx(produtos, outputPath) {
     'xl/workbook.xml': `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
           xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-  <sheets><sheet name="Produtos" sheetId="1" r:id="rId1"/></sheets>
+  <sheets><sheet name="Worksheet" sheetId="1" r:id="rId1"/></sheets>
 </workbook>`,
 
     'xl/_rels/workbook.xml.rels': `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
